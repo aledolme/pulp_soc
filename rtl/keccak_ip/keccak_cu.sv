f4d6e9246d30033d98da7d6395c076d08a484df3
mattia.mirigaldi <mattia.mirigaldi.98017@gmail.com>
2023-02-17 12:17:06 +0100

    Added optimized keccak architecture

--- rtl/keccak_ip/keccak_cu.sv
+++ rtl/keccak_ip/keccak_cu.sv
@@ -1,27 +1,17 @@
-module keccak_cu 
-    #(
-      localparam D_WIDTH=1600,
-      localparam D_KECCAK_WIDTH=64
-    )(
+module keccak_cu (
      input logic 		       clk_i,
      input logic 		       rst_ni,
      input logic 		       start_i,
-     input logic 		       ready_keccak_i, 
-     input logic [D_WIDTH-1:0] 	       din_i,
-     input logic [D_KECCAK_WIDTH-1:0]  dout_keccak_i,
-     input logic 		       dout_valid_keccak_i,
-     output logic 		       start_keccak_o,
-     output logic 		       last_block_keccak_o, 
-     output logic [D_WIDTH-1:0]	       dout_o,
-     output logic [D_KECCAK_WIDTH-1:0] din_keccak_o,
-     output logic 		       din_valid_keccak_o,
+     input logic 		       ready_dp_i, 
+     output logic 		       start_dp_o,
      output logic 		       status		       
    );
 
 
-   parameter S_0 = 0, S_1 = 1,
-             S_2 = 2, S_3 = 3;
-
+   parameter wait_start = 0,
+             do_permutation = 1,
+             permutation_finished = 2;
+   
    reg[4:0]  			       counter;
    //shortint unsigned		       counter;
  			       
@@ -31,35 +21,14 @@ module keccak_cu
    // State reg
    always_ff @(posedge clk_i) begin
       if ( !rst_ni) begin
-	 State <= S_0;
+	 State <= wait_start;
 	 counter <= 0;
       end else begin 
-	if ( State != State_next || State == S_0) begin
-	   //counter <= 0;
-	   if ( State_next == S_1) begin
-	      din_keccak_o <= din_i[D_KECCAK_WIDTH*counter +: D_KECCAK_WIDTH];
+	if ( State_next == do_permutation) begin
 	      counter <= counter+1;
-	   //end else if (State_next == S_3) begin
-	   //   dout_o[0 +: D_KECCAK_WIDTH] <= dout_keccak_i;
-	   //   counter <= 1;
-	   end else begin 
-	      counter <= 0;
-	   end
 	end else begin
-	   counter <= counter + 1;   //NOTE : concurrent process, counter seen by bit slicing is not the updated one 
-	   if ( State == S_1) begin
-	      if ( counter != 25) begin
-		 din_keccak_o <= din_i[D_KECCAK_WIDTH*counter +: D_KECCAK_WIDTH];
-	      end else begin
-		 din_keccak_o <= 0;
-	      end
-	   end else if (State == S_3 ) begin
-	      dout_o[D_KECCAK_WIDTH*counter +: D_KECCAK_WIDTH] <= dout_keccak_i;
-	   end else begin
-	      din_keccak_o <= 0;
-	      dout_o <= 0;
-	   end   
-	end
+	   counter <= 0;
+	end 
 	State <= State_next;
       end
    end 
@@ -67,66 +36,34 @@ module keccak_cu
    // Comb logic      
    always_comb begin
       case (State)
-	S_0 : begin
-	   din_valid_keccak_o <= 0;
-	   last_block_keccak_o <= 0;
-	   if (start_i && ready_keccak_i) begin
-	      start_keccak_o <= 1;
-	      State_next <= S_1;
+	wait_start : begin
+	   if (start_i && ready_dp_i) begin
+	      start_dp_o <= 1;
+	      State_next <= do_permutation;
 	   end else begin
-	      start_keccak_o <= 0;
-	      State_next <= S_0;	      
+	      start_dp_o <= 0;
+	      State_next <= wait_start;	      
 	   end
 	end
-	S_1 : begin
-	   start_keccak_o <= 0;
-	   din_valid_keccak_o <= 1;
-	   last_block_keccak_o <= 0;
+	do_permutation : begin
+	   start_dp_o <= 0;
 	   status <= 0;
-	   if (counter == 26) begin
+	   if (counter == 24) begin
 	      //din_keccak_o <= 0;
-	      State_next <= S_2;
+	      State_next <= permutation_finished;
 	   end else begin
-	      State_next <= S_1;
+	      State_next <= do_permutation;
 	   end
         end
-	S_2 : begin
-	   start_keccak_o <= 0;
-	   din_valid_keccak_o <= 0;
-	   //din_keccak_o <= 0;
-	   status <= 0;
-	   if (counter == 25) begin
-	      last_block_keccak_o <= 1;
-	   end else begin
-	      last_block_keccak_o <= 0;
-	   end    
-	   if ( counter == 26) begin
-	      State_next <= S_3;
-	   end else begin
-	      State_next <= S_2; 
-	   end
+	permutation_finished : begin
+	   start_dp_o <= 0;
+	   status <= 1;
+	   State_next <= wait_start;
         end 
-	S_3 : begin
-	   start_keccak_o <= 0;
-	   din_valid_keccak_o <= 0;
-	   //din_keccak_o <= 0;
-	   last_block_keccak_o <= 0;
-	   if (counter == 25) begin
-	      State_next <= S_0;
-	       status <= 1;
-	   end else begin
-	      State_next <= S_3;
-	   end
-	end
 	default : begin
-	   start_keccak_o <= 0;
-	   //dout_o <='{default: '0};
-	   din_valid_keccak_o <= 0;
-	   //din_keccak_o <= 0;
-	   last_block_keccak_o <= 0;
-	   State_next <= S_0;
+	   start_dp_o <= 0;
 	   status <= 0;
-	   
+	   State_next <= wait_start;
 	end
    endcase
    end // always_comb @
